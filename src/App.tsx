b06baac98ea64c791842b060b177c9acbe6c9bc5
abhitech2002 <oabhishek31@gmail.com>
2024-09-15 15:50:42 +0530

    Updates on path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import OTPVerification from './components/Auth/OTPVerification';
 import WelcomePage from './pages/WelcomePage';
 import Login from './components/Auth/Login';
@@ -11,6 +11,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signup />} />
         <Route path="/otp-verification" element={<OTPVerification />} />
